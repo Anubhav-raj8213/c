edbc7834e4f870daba3ee479ac00dd8d2a789238
root <root@localhost>
2023-01-08 17:44:44 +0530

    auto publish 1673180083678

--- src/main.c
+++ src/main.c
@@ -1,18 +1,5 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
-     {
-       int i,j,n,k;
-       printf("Enter the number of terms:\n");
-       scanf("%d", &n);
-       i=1;
-       j=0;
-       k=1;
-       while(i<=n){
-         while(k<=i){
-          j=j+k*k;
-          k++;
-         }
-         i++;
-       }
-       printf("The sum is: %d\n", j);
-     }
+    {
+      int i
+    }

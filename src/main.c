778fdefb612471967c684d93c8a530b3a72104cb
root <root@localhost>
2023-02-04 15:50:46 +0530

    auto publish 1675506045738

--- src/main.c
+++ src/main.c
@@ -1,5 +1,15 @@
-#include <stdio.h>
-int main()
-    {
-      int i
+#include<stdio.h>
+int main(){
+   int marks[10],i,j;
+    printf("Enter the marks roll numberwise:\n");
+    for(i=0;i<10;i++){
+      printf("Enter the marks of roll no. %d\n", i+1);
+      scanf("%d\n", &marks[i]);
     }
+      for(i=0;i<10;i++){
+        if(marks[i] < 35){
+          printf("Roll no: %d is fail .\n", i+1);
+        }
+      }
+      return 0;
+}
